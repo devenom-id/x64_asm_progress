2d38314918d4e35466ddd0a93404c7f0fd45f5a0
devenom-id <hu.devenom@gmail.com>
2024-09-05 16:15:34 -0300

    Updated itoa

--- ReplaceWithAlphabetPosition/itoa.asm
+++ ReplaceWithAlphabetPosition/itoa.asm
@@ -2,48 +2,56 @@ section .text
   global _start
 
 digcount_1:
-  div 10
+  div rsi
   inc rcx
-  cmp rdi,0
+  cmp rax,0
+  mov rdx,0
   jne digcount_1
   ret
 
 digcount:
   mov rax,rdi
   mov rcx,0
-  cmp rdi,0
+  mov rdx,0
+  mov rsi,10
   call digcount_1
+  mov rax,rcx
   ret
 
 itoa_2:
-  mov rcx,0
-  div rax,10
-  mov [rsp+16+rsi-1], cl
-  cmp rsi,0
+  mov rdx,0
+  div rcx
+  add dl,48
+  mov [rsp+16+rsi-1], dl
+  cmp rax,0
+  dec rsi
   jne itoa_2
   ret
 itoa:  ; args: (int n)
   call digcount
   mov rsi,rax  ; rsi: digcount
+  mov r9,rax
   inc rsi
   mov rcx, qword [rsp]
   sub rsp, rsi
   dec rsi
-  mov [rsp], rcx
+  mov qword [rsp], rcx
   mov rax,rdi
+  mov rcx,10
   call itoa_2
-  mov [rsp+16+rsi], 0
+  mov byte [rsp+8+r9], 0
+  mov rax, rsp
+  add rax,8
   ret
 
 strlen_1:
-  cmp [rdi+rax],0
   inc rax
+  cmp byte [rdi+rax],0
   jne strlen_1
   ret
 strlen:
-  mov rax,0
+  mov rax,-1
   call strlen_1
-  dec rax
   ret
 
 _start:
@@ -56,6 +64,10 @@ _start:
 
   mov rax,1
   mov rdi,1
+  syscall
+  mov rax,1
+  mov byte [rsi], 10
+  mov rdx,1
   syscall
   mov rax,60
   mov rdi,0

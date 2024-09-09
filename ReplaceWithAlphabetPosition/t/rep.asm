218cbe85afafbb6b401761beb5cf9264d53a332d
devenom-id <hu.devenom@gmail.com>
2024-09-09 04:45:49 -0300

    progress

--- ReplaceWithAlphabetPosition/t/rep.asm
+++ ReplaceWithAlphabetPosition/t/rep.asm
@@ -1,130 +1,123 @@
 section .text
   global main
   extern printf
-  extern stpcpy
   extern strlen
-
+  extern stpcpy
 digcount_1:
+  mov rsi, 10
   mov rdx, 0
-  div rdi
+  div rsi
   inc rcx
   cmp rax, 0
   jne digcount_1
   ret
 digcount:
-  ; args: int n
-  ; returns: int
-  ; modifies: rcx,rdi
-  mov rax,rdi
-  mov rdi,10
+  ; (int n) -> (char*)
+  ; rcx,rsi
   mov rcx,0
+  mov rax,rdi
   call digcount_1
   mov rax,rcx
   ret
-
-itoa_1:
-  dec r10
-  mov rdx,0
-  div r9
-  add rdx,48
-  mov byte [rbp+r10], dl
-  cmp r10,0
-  jg itoa_1
-  ret
 itoa:
-  ; args: int n
-  ; returns: char*
-  ; modifies: r9,r10,rbp
-  push rdi
+  ; (int n) -> (char*)
+  ; rcx,rsi
+  push rsp
+  push rbp
+  ; get number of digits
   call digcount
-  pop rdi
+  mov rsi,rax
 
-  mov r9, qword [rsp]
-  mov r10,rax ; r10 = digcount(n)
-  sub rsp, r10
-  dec rsp
-  mov qword [rsp], r9
-  sub rsp,8  ; push rbp
-  mov qword [rsp], rbp
-  mov rbp,rsp
-  add rbp,16
-  mov byte [rbp+r10], 0
+  ; stack ops
+  inc rsi
+  sub rsp, rsi
+  mov rcx, qword [rsp+rsi]
+  mov qword [rsp], rcx
+  mov rcx, qword [rsp+rsi+8]
+  mov qword [rsp+8], rcx
+  mov rcx, qword [rsp+rsi+16]
+  mov qword [rsp+16], rcx
+  mov rbp, rsp
+  add rbp, 24
 
-  mov r9,10
-  mov rax,rdi
-  call itoa_1
-  mov rax,rbp
+  ; call for loop      ;;;;;; CONTINUE
+
   pop rbp
-  ret
-
+  pop rsp
 zerobuff:
-  ; args: void* ptr, int nmemb
-  ; returns: void
-  ; modifies:
-  mov [rdi+rsi], 0
-  dec rsi
+  ; (char* s, int size) -> (void)
+  ; rsi
+  mov byte [rbp+10+rsi], 0
   cmp rsi, 0
   jne zerobuff
-
-
-alph_1:
-  cmp byte [rdi+rcx],65
-  cmovge r10,r9
-  cmp byte [rdi+rcx],90
-  cmovle r11,r9
-  and r10,r11
-  jz alph_2
-  add byte [rdi+rcx], 32
-alph_2:
   ret
 
+alph_x:
 alph:
-  ; args: char* s
-  ; returns: char*
-  ; modifies: rdi, rsi, rdx
+  ; (char* s) -> (char*)
+  ; rsi,rcx
+  ; saving register
+  push rsp
+  push rbp
+
+  ; length of s
   mov rax,0
-  call strlen  ; rax=strlen(s);
-  mov rsi,3
-  mul rsi  ;rax*=3;
-  inc rax  ;rax++;
-
-  mov rdx, qword [rsp]
-  sub rsp,rax  ; char* buff = alloca(rax);
-  sub rsp,8  ; char* ptr = buff;
-  mov qword [rsp], rdx
-  sub rsp,8
-  mov qword [rsp], rbp ; push rbp
-  mov rbp,rsp
-  add rbp,16
-
-  mov rdi,rbp
-  add rdi,8
-  mov qword [rbp], rdi  ; ptr=buff;
+  call strlen
   mov rsi,rax
-  mov rdx,rax  ; rdx=rax:strlen(s);
-  call zerobuff  ; zerobuff(rdi, rsi);
-  dec rdx  ;rdx--;
-  mov rcx, 0
-  mov r9, 1
-  mov r10,0
-  mov r11,0
-  call alph_1
 
+  ; allocate space for variables
+  mov rcx, 3
+  mul rcx
+  inc rax
+  sub rsp, rax ; buffer
+  sub rsp, 10  ; char* ptr; int size;
+
+  ; reorder stack (rbp,rsp,main,size,ptr,buffer)
+  mov rcx, qword [rsp+rax+10]
+  mov qword [rsp], rcx
+  mov rcx, qword [rsp+rax+10+8]
+  mov qword [rsp+8], rcx
+  mov rcx, qword [rsp+rax+10+16]
+  mov qword [rsp+16], rcx
+  mov rbp, rsp
+  add rbp, 24
+
+  ; assign strlen to size
+  mov word [rbp], si
+
+  ; assign buffer address to ptr
+  mov rcx, qword [rbp+10]
+  mov qword [rbp+2], rcx
+
+  ; zero the buffer
+  mov rdi, [rbp+10]
+  inc rsi
+  call zerobuff
+  mov si, word [rbp]  ; recover rsi
+
+  ; for loop
+  call alph_x
+
+  ; restoring registers
+  pop rbp
+  pop rsp
 
 main:
+  ; aligning stack
   mov rbp,rsp
   sub rsp,24
-  mov rdi,333
-  call itoa
-  mov rsi,rax
-  mov rdi,fmt
-  mov rax,0
-  call printf
+
+  ; calling alph
+  mov rdi, msg
+  call alph
+
 _exit:
   mov rax,60
   mov rdi,0
   syscall
 
 section .data
-  msg db "Hello world",10,0
   fmt db "%s",10,0
+  msg db "Hello world",10,0
+  tst db "%d",10,0
+
